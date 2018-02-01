aeb630583741dc6d207e249e2a7ab7bf4c568e61
tenshotanaka <tensho1211@icloud.com>
2018-02-02 02:06:08 +0900

    feature

---
+++ app/src/main/java/com/example/tanakatensho/finalpresentation/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tanakatensho.finalpresentation;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.commit();
+    }
+
+    public void onJankenButtonTapped(View view){
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("MY_HAND", view.getId());
+        startActivity(intent);
+    }
+}
+
